93b240b69bd9949ef013e22ae6881ab5bce92194
Joost Diepenmaat <joost@zeekat.nl>
2020-09-29 11:40:34 +0200

    Include validator middleware and ooapi v4 spec

---
+++ policies/openapi-validator.js
@@ -0,0 +1,35 @@
+module.exports = {
+  name: "openapi-validator",
+  schema: {
+    $id: 'http://express-gateway.io/schemas/policies/openapi-validator.json',
+    type: 'object',
+    properties: {
+      apiSpec: {
+        type: 'string',
+        description: 'path to the OpenAPI specification to use'
+      },
+      validateRequests: {
+        type: 'boolean',
+        description: 'whether to validate requests'
+      },
+      validateResponses: {
+        type: 'boolean',
+        description: 'whether to validate responses'
+      }
+    },
+    required: ['apiSpec'],
+  },
+  policy: ({apiSpec, validateRequests, validateResponses}) => {
+    console.log("Validator: ",apiSpec, validateRequests, validateResponses);
+    const OpenApiValidator = require('express-openapi-validator');
+    console.log(OpenApiValidator);
+    const bodyParser = require("body-parser");
+
+    const {squashMiddlewareStack} = require("../lib/utils.js");
+    const middlewareStack =  OpenApiValidator.middleware( {apiSpec, validateRequests, validateResponses} );
+    middlewareStack.unshift(bodyParser.urlencoded({extended: false}));
+    middlewareStack.unshift(bodyParser.text());
+    middlewareStack.unshift(bodyParser.json());
+    return squashMiddlewareStack(middlewareStack);
+  }
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const gateway = require('express-gateway');
 
 const policies = require('express-gateway/lib/policies');
 policies.register(require('./policies/example'));
+policies.register(require('./policies/openapi-validator'));
 
 gateway()
   .load(path.join(__dirname, 'config'))
