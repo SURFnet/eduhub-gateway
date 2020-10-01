d2581580138a4ea2e28f74daf60ef1f703de3efd
Remco van 't Veer <remco@remworks.net>
2020-10-01 11:12:26 +0200

    Drop axios dependency for tests
    Use nodejs standard `http` package instead.

--- test/example-integration.test.js
+++ test/example-integration.test.js
@@ -1,12 +1,12 @@
-const axios = require('axios');
+const http = require('http');
 const assert = require('assert');
 const path = require('path');
 const {DockerComposeEnvironment} = require('testcontainers');
 
-const httpGet = async (url, headers = {}) => {
-  let res;
-  try { res = await axios.get(url, headers); } catch (err) { res = err.response; }
-  return res;
+const httpGet = (url, opts) => {
+  return new Promise(
+    (resolve, reject) => http.get(url, opts, res => resolve(res))
+  );
 }
 
 const skipThisTest = process.env.MOCKA_SKIP == 'integration';
@@ -36,12 +36,12 @@ describe('example integration with container', () => {
   it('should respond with 400 without example header', async () => {
     const port = gwContainer.getMappedPort(8080);
     const res = await httpGet(`http://localhost:${port}/example`);
-    assert.equal(res.status, 400);
+    assert.equal(res.statusCode, 400);
   });
 
   it('should respond with 200 with example header', async () => {
     const port = gwContainer.getMappedPort(8080);
     const res = await httpGet(`http://localhost:${port}/example`, {headers: {Example: true}});
-    assert.equal(res.status, 200);
+    assert.equal(res.statusCode, 200);
   });
 });
