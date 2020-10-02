230b2ec86937d0087edc6b61d0cd1a6a8973ce5e
Joost Diepenmaat <joost@zeekat.nl>
2020-10-02 09:07:50 +0200

    Validation middleware (#4)
    Include validator middleware and ooapi v4 spec

---
+++ policies/openapi-validator.js
@@ -0,0 +1,35 @@
+const logger = require('express-gateway/lib/logger').createLoggerWithLabel("[OAGW:Validator]");
+const validator = require('express-openapi-validator');
+const bodyParser = require("body-parser");
+const {squashMiddlewareStack} = require("../lib/utils.js");
+
+module.exports = {
+  name: "openapi-validator",
+  schema: {
+    $id: 'http://express-gateway.io/schemas/policies/openapi-validator.json',
+    type: 'object',
+    properties: {
+      apiSpec: {
+        type: 'string',
+        description: 'path to the OpenAPI specification to use'
+      },
+      validateRequests: {
+        type: 'boolean',
+        description: 'whether to validate requests'
+      },
+      validateResponses: {
+        type: 'boolean',
+        description: 'whether to validate responses'
+      }
+    },
+    required: ['apiSpec'],
+  },
+  policy: ({apiSpec, validateRequests, validateResponses}) => {
+    logger.info("Instantiating validator",apiSpec, validateRequests, validateResponses);
+    const middlewareStack =  validator.middleware( {apiSpec, validateRequests, validateResponses} );
+    middlewareStack.unshift(bodyParser.urlencoded({extended: false}));
+    middlewareStack.unshift(bodyParser.text());
+    middlewareStack.unshift(bodyParser.json());
+    return squashMiddlewareStack(middlewareStack);
+  }
+}
