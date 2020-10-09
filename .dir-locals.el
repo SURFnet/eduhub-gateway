f87fccd40ee3ef2d793b5cdba7bd01dcc3157055
Joost Diepenmaat <joost@zeekat.nl>
2020-10-09 10:32:49 +0200

    Enable `standardjs` linter

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,9 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((js-mode
-  (js-indent-level . 2)))
+  (js-indent-level . 2))
+ (js2-mode
+  (js2-strict-missing-semi-warning . nil))
+ ;; use provided `standard` checker for javascript
+ (nil . ((eval . (setq flycheck-javascript-standard-executable
+                       (concat (projectile-project-root) "/node_modules/.bin/standard"))))))
