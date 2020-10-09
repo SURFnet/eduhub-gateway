8d495e5da7e2444ddc04d6ea9409289527ec9c0b
Joost Diepenmaat <joost@zeekat.nl>
2020-10-09 10:33:29 +0200

    Various linting fixes

--- policies/gatekeeper/gatekeeper.js
+++ policies/gatekeeper/gatekeeper.js
@@ -1,18 +1,17 @@
-const authentication = require('./authentication');
-const credentials = require('./credentials');
+const authentication = require('./authentication')
+const credentials = require('./credentials')
 
 const realm =
       process.env.SURFNET_OOAPI_GW_CLIENT_REALM ||
-      'SURFnet OOAPI Gateway client access';
+      'SURFnet OOAPI Gateway client access'
 
-module.exports = (params) => (
+module.exports = (params) =>
   (req, res, next) => {
     if (authentication.appFromRequest(req, credentials.read())) {
-      delete req.headers.authorization;
-      next();
+      delete req.headers.authorization
+      next()
     } else {
-      res.set({'WWW-Authenticate': `Basic realm="${realm}"`});
-      res.status(401).send('Unauthorized');
+      res.set({ 'WWW-Authenticate': `Basic realm="${realm}"` })
+      res.status(401).send('Unauthorized')
     }
   }
-)
--- test/example.integration.test.js
+++ test/example.integration.test.js
@@ -1,38 +1,38 @@
-const assert = require('assert');
-const {httpGet, gwContainer, integrationContext} = require('./integration.environment.js');
+const assert = require('assert')
+const { httpGet, gwContainer, integrationContext } = require('./integration.environment.js')
 
 // As reflected in config/credentials.json.test
-const testCredentials = 'test:ea8631510aaa7dd11b734fd9ce167d0a';
+const testCredentials = 'test:ea8631510aaa7dd11b734fd9ce167d0a'
 
-integrationContext('example policy', function() {
+integrationContext('example policy', function () {
   it('should respond with 401 without credentials', async () => {
-    const port = gwContainer().getMappedPort(8080);
-    const res = await httpGet(`http://localhost:${port}/example`);
-    assert.equal(res.statusCode, 401);
-  });
+    const port = gwContainer().getMappedPort(8080)
+    const res = await httpGet(`http://localhost:${port}/example`)
+    assert.strictEqual(res.statusCode, 401)
+  })
 
   it('should respond with 401 with bad credentials', async () => {
-    const port = gwContainer().getMappedPort(8080);
+    const port = gwContainer().getMappedPort(8080)
     const res = await httpGet(`http://localhost:${port}/example`, {
       auth: 'bad:credentials'
-    });
-    assert.equal(res.statusCode, 401);
-  });
+    })
+    assert.strictEqual(res.statusCode, 401)
+  })
 
   it('should respond with 400 with auth but without example header', async () => {
-    const port = gwContainer().getMappedPort(8080);
+    const port = gwContainer().getMappedPort(8080)
     const res = await httpGet(`http://localhost:${port}/example`, {
       auth: testCredentials
-    });
-    assert.equal(res.statusCode, 400);
-  });
+    })
+    assert.strictEqual(res.statusCode, 400)
+  })
 
   it('should respond with 200 with auth and example header', async () => {
-    const port = gwContainer().getMappedPort(8080);
+    const port = gwContainer().getMappedPort(8080)
     const res = await httpGet(`http://localhost:${port}/example`, {
       auth: testCredentials,
-      headers: {Example: true}
-    });
-    assert.equal(res.statusCode, 200);
-  });
-});
+      headers: { Example: true }
+    })
+    assert.strictEqual(res.statusCode, 200)
+  })
+})
