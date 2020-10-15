afb39b8a334bebf5f0fe1cb9c42a7900c51da2a8
Joost Diepenmaat <joost@zeekat.nl>
2020-10-15 09:19:31 +0200

    Validate responses
    Improve documentation for middleware
    
    Smaller more explicitly versioned base for invalid-backend docker image
    
    Well formed response for request validation failures
    
    Renamed invalid-backend to test-backend and dropped ooapi-mock
    container.  The test-backend provides valid and invalid responses.
    
    There's a course with id 900d900d-900d-900d-900d-900d900d900d which is
    well formed and a badbadba-badb-badb-badb-badbadbadbad course which is
    missing the required "name" attribute.

--- policies/openapi-validator.js
+++ policies/openapi-validator.js
@@ -5,6 +5,8 @@ const fs = require('fs')
 const jsYaml = require('js-yaml')
 const { squashMiddlewareStack } = require('../lib/utils.js')
 const modifyResponse = require('express-modify-response')
+const { oasPathToExpressPath } = require('express-openapi-validate/dist/schema-utils')
+const pathToRegexp = require('path-to-regexp')
 
 module.exports = {
   name: 'openapi-validator',
@@ -22,7 +24,7 @@ module.exports = {
       },
       validateResponses: {
         type: 'boolean',
-        description: 'whether to validate responses'
+        description: 'whether to validate responses. If true, and the request includes an `X-Validate-Response true` header, the response will be validated'
       }
     },
     required: ['apiSpec']
@@ -32,27 +34,53 @@ module.exports = {
     const openApiDocument = jsYaml.safeLoad(
       fs.readFileSync(apiSpec, 'utf-8')
     )
-    const validator = new OpenApiValidator(openApiDocument)
+    const validator = new OpenApiValidator(openApiDocument, { ajvOptions: { coerceTypes: true } })
     const middlewareStack = []
     if (validateRequests) {
-      middlewareStack.push(validator.match())
+      middlewareStack.push(((handler) =>
+        (req, res, next) => {
+          handler(req, res, (err) => {
+            if (err instanceof ValidationError) {
+              res.setHeader('content-type', 'application/json')
+              res.status(400) // Bad Request
+                .send(JSON.stringify({ message: err.message, data: err.data }))
+            } else if (err instanceof Error) {
+              res.setHeader('content-type', 'text/plain')
+              res.status(500) // Internal error
+                .send(err.message)
+            } else {
+              next(err)
+            }
+          })
+        }
+      )(validator.match()))
     }
     if (validateResponses) {
+      const paths = Object.keys(validator._document.paths).map(path => ({
+        path,
+        regex: pathToRegexp(oasPathToExpressPath(path))
+      }))
       middlewareStack.push(
         modifyResponse(
           (req, res) => {
             // This should return true if the response body is to be validated
-            return true
+            return req.headers['x-validate-response'] === 'true'
           },
           (req, res, body) => {
             try {
-              validator.validateResponse(req.method.toLowerCase(), req.path)(
-                {
-                  statusCode: res.statusCode,
-                  headers: res.getHeaders(),
-                  body: JSON.parse(body.toString())
-                }
-              )
+              // find request path in schema document
+              const match = paths.find(({ regex }) => regex.test(req.path))
+              if (match) {
+                validator.validateResponse(req.method.toLowerCase(), match.path)(
+                  {
+                    statusCode: res.statusCode,
+                    headers: res.getHeaders(),
+                    body: JSON.parse(body.toString())
+                  }
+                )
+              } else {
+                return body
+              }
             } catch (e) {
               if (e instanceof ValidationError) {
                 res.statusCode = 502 // Bad Gateway
