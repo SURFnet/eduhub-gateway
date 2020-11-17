3bfb4fe99975dc57d40c79221c9fbb0b813a99a6
Remco van 't Veer <remco@remworks.net>
2020-11-17 16:28:15 +0100

    GPLv3

--- policies/gatekeeper/gatekeeper.js
+++ policies/gatekeeper/gatekeeper.js
@@ -1,3 +1,19 @@
+/* Copyright (C) 2020 SURFnet B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
 const logger = require('express-gateway/lib/logger').createLoggerWithLabel('[OAGW:Gatekeeper]')
 
 const httpcode = require('../../lib/httpcode')
