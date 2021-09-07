75d8e1fb207a82e0677017385ebdfd8ea7587e52
Joost Diepenmaat <joost@zeekat.nl>
2021-09-07 10:42:03 +0200

    Fix ID of test backend in curl example

--- dev/observability/test-request.sh
+++ dev/observability/test-request.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -v -u fred:96557fbdbcf0ac9d83876f17165c0f16 -H "X-Route: endpoint=TestBackend" http://localhost:8081/courses
+curl -v -u fred:96557fbdbcf0ac9d83876f17165c0f16 -H "X-Route: endpoint=Test.Backend" 'http://localhost:8081/courses'|jq
 
 
 #  echo -n '{ "version": "1.1", "host": "example.org", "short_message": "A short message", "level": 5, "_some_info": "foo" }' | nc -w0  localhost 12201
