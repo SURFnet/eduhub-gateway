67d8a56afeae01fa0c23b6751c42fb489a82b5bb
Jelmer de Ronde <jelmer.deronde@surf.nl>
2022-08-05 09:22:12 +0200

    Fix linting errors

--- healthcheck.js
+++ healthcheck.js
@@ -1,23 +1,23 @@
-var http = require("http");
+const http = require('http')
 
-var options = {
-  host: "localhost",
-  port: "8080",
-  timeout: 1000,
-};
+const options = {
+  host: 'localhost',
+  port: '8080',
+  timeout: 1000
+}
 
-var request = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  if (res.statusCode == 401) {
-    process.exit(0);
+const request = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`)
+  if (res.statusCode === 401) {
+    process.exit(0)
   } else {
-    process.exit(1);
+    process.exit(1)
   }
-});
+})
 
-request.on("error", function (err) {
-  console.log("ERROR");
-  process.exit(1);
-});
+request.on('error', function (err) {
+  console.log(err)
+  process.exit(1)
+})
 
-request.end();
+request.end()
